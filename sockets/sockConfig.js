ffa65bb4d87f2506bdf8ba06f3032873a36d703e
astics <nikunjblu.tandel@gmail.com>
2017-10-25 19:52:24 +0530

    message module added

--- sockets/sockConfig.js
+++ sockets/sockConfig.js
@@ -5,9 +5,9 @@ module.exports = {
     start: function(io) {
         io.on('connection', function(socket) {
             socket.on('message', function(message) {
-                console.log('from console  :',message);
+                console.log('from console  :',message.message);
                 io.emit('message',{
-                    message : message
+                    message : message.message
                 });
             });
         });
