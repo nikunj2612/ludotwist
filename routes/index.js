ce817afd4952f3b23b000c40cfe65dc9b98f5eab
astics <nikunjblu.tandel@gmail.com>
2017-10-25 14:20:48 +0530

    test API added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
-
+router.get('/test', function(req, res, next) {
+    res.send({status : 1});
+});
 module.exports = router;
